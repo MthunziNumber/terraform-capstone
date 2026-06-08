68c652d42ebc66098bb3d61626a165638d6a1456
Thulani_Mthunzi_Maseko <thulanimthunzimaseko@yahoo.co.za>
2026-06-08 13:26:04 -0700

    Add AWS provider and resources for VPC and subnet

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
-provider "AWS" { region = var.aws_region}
+provider "aws" { 
+                  region = var.aws_region
+               }
+
+resource "aws_vpc" "main" { 
+                            cidr_block = "10.0.0.0/16"
+                          }
+
+resource "aws_subnet" "public" { 
+                                 vpc_id            = aws_vpc.main.id
+                                 cidr_block        = "10.0.1.0/24"
+                                 availability_zone = "${var.aws_region}a"
+                               }
+
+resource "aws_instance" "web" { 
+                                ami           = "ami-0c55b159cbfafe1f0" 
+                                instance_type = var.instance_type
+                                subnet_id     = aws_subnet.public.id
+                              }
